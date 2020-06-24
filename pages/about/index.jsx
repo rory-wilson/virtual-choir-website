5765c5cfeac0f62b6e7d7630a58712a193e76bb1
Rory Wilson <rorywilson@Rorys-MacBook-Pro.local>
2020-06-24 23:29:20 +0100

    updated iain's bio

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -22,20 +22,8 @@ function AboutPage() {
                     <h2>About the conductor</h2>
                     <img src="/images/iain.jpg" align="left" alt="Photograph of Iain McLarty, Conductor" style={{ width: 300, 'margin-right': 10 }} />
                     <p>
-                        Scottish conductor <a target="_blank" href="https://www.iainmclarty.co.uk/">Iain McLarty</a> studied in the Soloist Class at
-                        the Royal Danish Academy of Music (DKDM) in Copenhagen
-                        with Giordano Bellincampi and Michael Schønwandt, concluding in a concert with Aarhus Symfoniorkester which included the Danish première of James MacMillan’s “The Confession of Isobel
-                        Gowdie”. He has also conducted many of the professional Danish orchestras and assisted for productions with the Royal Danish Opera Academy. He is committed to performing contemporary music, giving numerous premières in both Scandinavia and the UK,
-                        and has taken part in masterclasses with the London Sinfonietta, Norwegian Radio Orchestra and BBC Singers.
-</p><p>
-                        In Scotland he has worked with groups across the country,
-                        performing much of the standard orchestral and choral repertoire as well as such diverse pieces as a concerto for birds and orchestra, choral works for forty individual voices, and a new opera based on reality TV. He is currently Musical Director of the Scottish Chamber Choir and Conductor of the St Andrews Symphony Orchestra as well as a regular guest conductor with the Edinburgh University String Orchestra. He has previously held positions as Sir Alexander Gibson Conducting Fellow with the Royal Scottish National Orchestra Chorus and Herrick Bunney Organ Scholar at St Giles’ Cathedral, Edinburgh.
-</p><p>
-                        Before taking up conducting he studied Mathematics and Music at the University of Edinburgh
-                        with his interest in two such seemingly contrasting fields leading to his featuring in the film “Le pere di Adamo” which was shown at the Rome Film Festival. He also has a Masters in Musicology, with his research focusing on classical and church music in Scotland. As a church musician his work covers a broad range of styles and he is active in worship development. He is
-                        particularly interested in music and liturgy in an ecumenical context and is regularly involved in planning and leading worship at various national and international conferences.
-
-</p>
+                        Scottish conductor <a target="_blank" href="https://www.iainmclarty.co.uk/">Iain McLarty</a>studied in the Soloist Class at the Royal Danish Academy of Music (DKDM) in Copenhagen conducting many of the professional Danish orchestras and assisting for productions with the Royal Danish Opera Academy. He is committed to performing contemporary music, giving numerous premières in both Scandinavia and the UK, and has taken part in masterclasses with the London Sinfonietta, Norwegian Radio Orchestra and BBC Singers. In Scotland he has worked with groups across the country, performing much of the standard orchestral and choral repertoire as well as such diverse pieces as a concerto for birds and orchestra, choral works for forty individual voices, and a new opera based on reality TV.
+                        </p><p>He is currently Musical Director of the Scottish Chamber Choir and Conductor of the St Andrews Symphony Orchestra as well as a regular guest conductor with the Edinburgh University String Orchestra. He has previously held positions as Sir Alexander Gibson Conducting Fellow with the Royal Scottish National Orchestra Chorus and Herrick Bunney Organ Scholar at St Giles’ Cathedral, Edinburgh.</p>
                 </Col>
             </Row>
         </Container>
